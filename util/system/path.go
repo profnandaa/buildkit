ae14ad69ed95b4048647bda8066bb9d4cec009bb
Anthony Nandaa <profnandaa@gmail.com>
2025-03-26 10:29:47 +0300

    wcow: leave default windows path empty
    Signed-off-by: Anthony Nandaa <profnandaa@gmail.com>

--- util/system/path.go
+++ util/system/path.go
@@ -12,10 +12,9 @@ import (
 // ':' character .
 const DefaultPathEnvUnix = "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
-// DefaultPathEnvWindows is windows style list of directories to search for
-// executables. Each directory is separated from the next by a colon
-// ';' character .
-const DefaultPathEnvWindows = "c:\\Windows\\System32;c:\\Windows;C:\\Windows\\System32\\WindowsPowerShell\\v1.0"
+// DefaultPathEnvWindows is left empty to be loaded from the registry
+// during container run.
+const DefaultPathEnvWindows = ""
 
 func DefaultPathEnv(os string) string {
 	if os == "windows" {
